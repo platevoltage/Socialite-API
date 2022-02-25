eaa357f4a8220a3cbd699605fbb56730bb0c630b
antieatingactivist <antieatingactivist@gmail.com>
2022-02-24 21:30:44 -0800

    created updateuser route

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,6 +17,21 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body }
+
+    )
+      // .select('')
+      // .populate('')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
   createUser(req, res) {
     User.create(req.body)
       .then((dbUserData) => res.json(dbUserData))
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { getAllUsers, createUser, getSingleUser } = require('../../controllers/userController');
+const { getAllUsers, createUser, getSingleUser, updateUser } = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getAllUsers).post(createUser);;
-router.route('/:userId').get(getSingleUser);
+router.route('/').get(getAllUsers).post(createUser);
+router.route('/:userId').get(getSingleUser).put(updateUser);
 
 module.exports = router;
