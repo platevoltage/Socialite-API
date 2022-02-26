19b0fc89f5ea7e5ee866f42c6b9ec537ab356384
antieatingactivist <antieatingactivist@gmail.com>
2022-02-25 21:21:03 -0800

    cleaned up code formatting

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
-const { getAllUsers, createUser, getSingleUser, updateUser, deleteUser, addFriend, deleteFriend } = require('../../controllers/userController');
+const { 
+    getAllUsers, 
+    createUser, 
+    getSingleUser, 
+    updateUser, 
+    deleteUser, 
+    addFriend, 
+    deleteFriend 
+} = require('../../controllers/userController');
 
-// /api/users
+
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
-// Require model
-// const { xxx } = require('./models');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
